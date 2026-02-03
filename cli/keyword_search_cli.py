022232e1fb633963d266946ee91eeb8f29655e52
Patrick Callahan <patricktcallahan18@gmail.com>
2026-02-03 19:27:41 +0100

    Include args for building index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 import argparse
-from helpers import load_movies, normalize_text, load_stop_words
+from helpers import load_movies, normalize_text, load_stop_words, build_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    # search command
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    # build command
+    subparsers.add_parser("build", help="Build the inverted index")
+
+
+
     args = parser.parse_args()
-
-
-
-
-
 
     movies = load_movies("data/movies.json")
     stop_words = load_stop_words("data/stopwords.txt")
@@ -23,6 +24,8 @@ def main() -> None:
 
 
     match args.command:
+        case "build":
+            build_command()
         case "search":
             # print the search query here
             counter = 1
