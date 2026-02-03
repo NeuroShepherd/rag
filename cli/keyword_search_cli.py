9ddad73387cb0cd62c891ec8f81dc43ddae9e08b
Patrick Callahan <patricktcallahan18@gmail.com>
2026-02-03 17:00:40 +0100

    Create basic parser to search movies

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    movies = load_movies("data/movies.json")
+
+
+    match args.command:
+        case "search":
+            # print the search query here
+            counter = 1
+            print(f"Searching for: {args.query}")
+            # ic(f"Searching for: {args.query}")
+            for movie in (movies):
+                if args.query.lower() in movie["title"].lower():
+                    print(f"{movie['id']}. Movie Title {movie['title']}")
+                    counter += 1
+                    if counter > 5:
+                        break
+        case _:
+            parser.print_help()
+
+            
+
+
+def load_movies(file_path: str) -> dict:
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return data["movies"]
+
+
+if __name__ == "__main__":
+    main()
