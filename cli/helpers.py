5cb13be50cf8634abfe7e9c05c3d51612b25483c
Patrick Callahan <patricktcallahan18@gmail.com>
2026-02-03 17:42:13 +0100

    Remove stop words from titles too

--- cli/helpers.py
+++ cli/helpers.py
@@ -10,8 +10,16 @@ def load_movies(file_path: str) -> dict:
     return data["movies"]
 
 
-def normalize_text(text: str) -> list[str]:
+def normalize_text(text: str, stop_words: list[str] | None = None) -> list[str]:
     text = text.lower()
     text = text.translate(str.maketrans("","",string.punctuation))
     text = text.split()
+    if stop_words is not None:
+        text = [word for word in text if word not in stop_words]
     return text
+
+
+def load_stop_words(file_path: str) -> list[str]:
+    with open(file_path, "r") as f:
+        stop_words = [f.strip() for f in f.readlines()]
+    return stop_words
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from helpers import load_movies, normalize_text
+from helpers import load_movies, normalize_text, load_stop_words
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -12,7 +12,13 @@ def main() -> None:
 
     args = parser.parse_args()
 
+
+
+
+
+
     movies = load_movies("data/movies.json")
+    stop_words = load_stop_words("data/stopwords.txt")
 
 
 
@@ -24,8 +30,8 @@ def main() -> None:
             # breakpoint()
             for movie in (movies):
                 # breakpoint()
-                query_words = normalize_text(args.query)
-                title_words = normalize_text(movie["title"])
+                query_words = normalize_text(args.query, stop_words=stop_words)
+                title_words = normalize_text(movie["title"], stop_words=stop_words)
                 if any(q in t for q in query_words for t in title_words):
                     print(f"{movie['id']}. Movie Title {movie['title']}")
                     counter += 1
