e22061271435c2d1dba6ce28360a3bc5da874396
Patrick Callahan <patricktcallahan18@gmail.com>
2026-02-03 19:27:21 +0100

    Create class and fxn to index the movie data

--- cli/helpers.py
+++ cli/helpers.py
@@ -2,6 +2,8 @@
 import json
 import string
 from nltk.stem import PorterStemmer
+import os
+import pickle
 
 
 
@@ -27,3 +29,46 @@ def load_stop_words(file_path: str) -> list[str]:
     with open(file_path, "r") as f:
         stop_words = [f.strip() for f in f.readlines()]
     return stop_words
+
+
+class InvertedIndex():
+    def __init__(self) -> None:
+        self.index = {}
+        self.docmap = {}
+
+    def __add_document(self, doc_id, text):
+        tokens = normalize_text(text)
+        for token in tokens:
+            if token not in self.index:
+                self.index[token] = set()
+            self.index[token].add(doc_id)
+
+    def get_documents(self, token: str):
+        value = self.index.get(token, set())
+        return sorted(value)
+
+    def build(self, file_path: str = "data/movies.json"):
+        movies = load_movies(file_path)
+        for movie in movies:
+            doc_id = movie["id"]
+            self.docmap[doc_id] = movie
+            text = f"{movie['title']} {movie['description']}"
+            self.__add_document(doc_id, text)
+
+
+    def save(self):
+        os.makedirs("cache", exist_ok=True)
+        
+        with open("cache/index.pkl", "wb") as f:
+            pickle.dump(self.index, f)
+
+        with open("cache/docmap.pkl", "wb") as f:
+            pickle.dump(self.docmap, f)
+
+
+def build_command() -> None:
+    index = InvertedIndex()
+    index.build()
+    index.save()
+    docs = index.get_documents("merida")
+    print(f"First document for token 'merida' = {docs[0]}")
