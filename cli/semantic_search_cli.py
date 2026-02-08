ede1fe49d4562973350241c777767407bfb44dbb
Patrick Callahan <patricktcallahan18@gmail.com>
2026-02-08 17:18:12 +0100

    Create sematicsearch class and verify fxn

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -3,12 +3,21 @@
 #!/usr/bin/env python3
 
 import argparse
+from semantic_search import verify_model, SemanticSearch
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # verify model parser
+    verify_parser = subparsers.add_parser("verify", help="Verify that the semantic search model loads correctly")
+
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
